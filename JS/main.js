8afb49fcc3769b9fdab4e21887dfb250c8d2cd92
pack-sp <patrick.lusilva@gmail.com>
2020-11-04 22:05:06 -0300

    cria index.html e scripts em JS

---
+++ JS/main.js
@@ -0,0 +1,41 @@
+var d = new Date();
+alert(d.getDay());
+alert(d.getHours());
+
+//var count;
+//for (count = 1; count <= 5; count++);
+//{
+//  alert(count);
+//}
+
+//var count = 0;
+//while (count <= 5) {
+//console.log(count);
+//count++;
+//}
+
+//var idade = prompt('Qual é a sua idade?');
+//if (idade >= 18) {
+//alert('maior de idade');
+//}else{
+//alert('menor de idade');
+//}
+
+//var fruta = { nome: 'maçã', cor: 'Vermelha' };
+//console.log(fruta.cor);
+
+//var lista = ['maçã', 'pêra', 'laranja'];
+//lista.pop();
+//lista.push('uva');
+//console.log(lista.length);
+//console.log(lista.reverse());
+//console.log(lista.toString());
+//console.log(lista.join(' - '));
+
+//var nome = 'Patrick Lucinhuki';
+//var idade = 36;
+//var frase = 'São Paulo é o melhor time do mundo';
+//alert(nome + ' tem ' + idade + ' anos');
+//console.log(nome);
+//console.log(idade);
+//console.log(frase.toLocaleUpperCase());
